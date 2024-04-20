00b478f63aec66e62a502e8a15cf9b70be2dfd9c
Gautham Vanjre <vanjregautham@gmail.com>
2024-04-20 14:25:25 +0530

    fix: cleared all errors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import logger from "../utils/winston";
 
 const Hero = () => {
   return (
@@ -8,27 +7,13 @@ const Hero = () => {
         <h1 className="text-4xl font-bold mb-4">Welcome to AI NFT Minter</h1>
         <div className="space-x-4">
           <Link to={"/ImageGenerator"}>
-            <button
-              onClick={() =>
-                logger.info(
-                  "user clicked on generate image button in home page"
-                )
-              }
-              className=" bg-slate-100 hover:bg-slate-500 text-purple-500 font-bold py-2 px-4 rounded"
-            >
+            <button className=" bg-slate-100 hover:bg-slate-500 text-purple-500 font-bold py-2 px-4 rounded">
               Generate Image
             </button>
           </Link>
 
           <Link to={"/NFTMinter"}>
-            <button
-              onClick={() =>
-                logger.info(
-                  "user clicked on generate image button + nft mint in home page"
-                )
-              }
-              className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
-            >
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
               Generate + Mint Image
             </button>
           </Link>
